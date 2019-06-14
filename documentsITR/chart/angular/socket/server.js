b178a7666045431fe2ebeebc76a5f652be767fba
yadavanurag <anuragyadav13481@gmail.com>
2019-06-14 12:48:02 +0000

    14 June 12:47

--- documentsITR/chart/angular/socket/server.js
+++ documentsITR/chart/angular/socket/server.js
@@ -1,48 +1,29 @@
-var WebSocketServer = require('websocket').server;
-var http = require('http');
+const express = require('express'),
+      app = express(),
+      server = require('http').createServer(app);
+      io = require('socket.io')(server);
 
-var server = http.createServer((req, res) => {
-    console.log(`${new Date()} received request for ${req.url} `);
-    res.writeHead(202);
-    res.end('How are you');
-});
 
-var port = 8888;
-server.listen(port, '127.0.0.1', () => {
-    console.log(`Server is listening on http://localhost:${port}`);
-    console.log(server.address().address);
-});
 
-wsServer = new WebSocketServer({
-    httpServer: server,
-    autoAcceptConnections: true
-});
+app.use(express.static(__dirname + '/dist')); 
 
-function originIsAllowed(origin) {
-    return true;
-}
 
-wsServer.on('request', (request) => {
-    if (!originIsAllowed(req.origin)) {
-        request.reject();
-        console.log(`${new Date()} Connection from origin ${request.origin}`);
-    }
 
-    var connection = request.accept('echo-protocol', request.origin);
-    console.log(`${new Date()} connection accepted`);
-
-    connection.on('message', (message) => {
-        if (message.type === 'utf8') {
-            console.log(`Received Message ${message.utf8Data}`);
-            connection.sendUTF(`Server Response\n ${message.utf8Data}`);
-        } else if (message.type === 'binary') {
-            console.log(`Received Binary Message of ${message.binaryData.length} length`);
-            connection.sendBytes(`Server Response\n ${message.binaryData}`);
-        }
+io.on('connection', (socket)=>{
+    socket.on('disconnect', ()=>{
+        console.log(`User ${socket.id} disconnected`);
     });
 
-    connection.on('close', (reasonCode, description) => {
-        console.log(`${new Data()} peer ${connection.remoteAddress} disconnected`);
+    socket.on('clientMessage', (clientData)=>{
+        console.log(clientData);
     });
 
+    socket.emit('serverMessage', {data: 'this is my data'});
+
 });
+
+
+server.listen(8888, '127.0.0.1', ()=>{
+    console.log('Visit http://127.0.0.1:8888 in your browser');
+});
+
---
+++ documentsITR/chart/angular/socketImplementation/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { SocketIoConfig, SocketIoModule } from 'ngx-socket-io';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+
+const config: SocketIoConfig = {url: 'http://127.0.0.1:8888', options: {}};
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SocketIoModule.forRoot(config)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
