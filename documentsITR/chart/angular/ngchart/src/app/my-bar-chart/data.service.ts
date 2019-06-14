a6cb5fa5b7c911d33d7f99e9b66f7de6fbc2aeb4
yadavanurag <anuragyadav13481@gmail.com>
2019-06-14 13:07:18 +0000

    14 Jun 13:07

--- documentsITR/chart/angular/ngchart/src/app/my-bar-chart/data.service.ts
+++ documentsITR/chart/angular/ngchart/src/app/my-bar-chart/data.service.ts
@@ -1,44 +1,76 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Observer } from 'rxjs';
-import { map, catchError } from 'rxjs/operators';
-import * as socketIo from 'socket.io-client';
+import { Socket } from 'ngx-socket-io';
 
-import { Socket } from '../shared/interfaces';
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService{
+  constructor(private socket: Socket){}
 
-declare var io : {
-  connect(url: string): Socket;
-};
-
-@Injectable()
-export class DataService {
-
-  socket: Socket;
-  observer: Observer<number>;
-
-  getData() : Observable<number> {
-    this.socket = socketIo('http://localhost:8080');
-
-    this.socket.on('data', (res) => {
-      this.observer.next(res.data);
-    });
-
-    return this.createObservable();
+  sendData(data: string){
+    this.socket.emit('clientData', data);
   }
-
-  createObservable() : Observable<number> {
-      return new Observable<number>(observer => {
-        this.observer = observer;
-      });
-  }
-
-  private handleError(error) {
-    console.error('server error:', error);
-    if (error.error instanceof Error) {
-        let errMessage = error.error.message;
-        return Observable.throw(errMessage);
-    }
-    return Observable.throw(error || 'Socket.io server error');
-  }
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { Injectable } from '@angular/core';
+// import { Observable } from 'rxjs';
+// import { Observer } from 'rxjs';
+// import { map, catchError } from 'rxjs/operators';
+// import * as socketIo from 'socket.io-client';
+
+// import { Socket } from '../shared/interfaces';
+
+// declare var io : {
+//   connect(url: string): Socket;
+// };
+
+// @Injectable()
+// export class DataService {
+
+//   socket: Socket;
+//   observer: Observer<number>;
+
+//   getData() : Observable<number> {
+//     this.socket = socketIo('http://localhost:8080');
+
+//     this.socket.on('data', (res) => {
+//       this.observer.next(res.data);
+//     });
+
+//     return this.createObservable();
+//   }
+
+//   createObservable() : Observable<number> {
+//       return new Observable<number>(observer => {
+//         this.observer = observer;
+//       });
+//   }
+
+//   private handleError(error) {
+//     console.error('server error:', error);
+//     if (error.error instanceof Error) {
+//         let errMessage = error.error.message;
+//         return Observable.throw(errMessage);
+//     }
+//     return Observable.throw(error || 'Socket.io server error');
+//   }
+
+// }
--- documentsITR/chart/angular/ngchart/src/app/my-bar-chart/my-bar-chart.component.ts
+++ documentsITR/chart/angular/ngchart/src/app/my-bar-chart/my-bar-chart.component.ts
@@ -8,28 +8,16 @@ import { DataService } from './data.service';
   templateUrl: './my-bar-chart.component.html',
   styleUrls: ['./my-bar-chart.component.css']
 })
-export class MyBarChartComponent implements OnInit {
+export class MyBarChartComponent implements OnInit, OnDestroy {
 
-  id: any;
-  sub: Subscription;
-  data: number;
+  constructor(private dataService: DataService){}
 
-  constructor(private dataService: DataService) { }
-  ngOnInit() {
-    this.id = setInterval(() => {
-      this.addData([Math.sin(Math.PI*(0.5-Math.random()))], new Date().toLocaleString());
-    }, 500);
-    // this.sub = this.dataService.getData()
-    //   .subscribe((data)=>{
-    //     this.data = data;
-    //     console.log(data);
-    //   });
+  myData:string;
+  ngOnInit(){
+    
   }
+  ngOnDestroy(){
 
-  ngOnDestroy() {
-    if (this.id) {
-      clearInterval(this.id);
-    }
   }
 
 
@@ -46,74 +34,77 @@ export class MyBarChartComponent implements OnInit {
 
 
 
+  // id: any;
+  // sub: Subscription;
+  // data: number;
+
+  // constructor(private dataService: DataService) { }
+  // ngOnInit() {
+  //   this.id = setInterval(() => {
+  //     this.addData([Math.sin(Math.PI*(0.5-Math.random()))], new Date().toLocaleString());
+  //   }, 500);
+  //   this.sub = this.dataService.getData()
+  //     .subscribe((data)=>{
+  //       this.data = data;
+  //       console.log(data);
+  //     });
+  // }
+
+  // ngOnDestroy() {
+  //   if (this.id) {
+  //     clearInterval(this.id);
+  //   }
+  // }
+
+
+  // public barChartOptions = {
+  //   scaleShowVerticalLines: false,
+  //   responsive: true, 
+  //   animation:{
+  //     duration: 250,
+  //     easing: 'linear'
+  //   },
+  //   scales: {
+  //     // xAxes: [{
+  //     //   type: 'time',
+  //     // }],
+  //     yAxes: [{
+  //       ticks: {
+  //         beginAtZero: true,
+  //         stepSize: 1
+  //       }
+  //     }]
+  //   }
+  // };
+  // public barChartLabels = ['first', 'second'];
+
+  // public barChartType = 'line';
+  // public barChartLegend = true;
+  // public barChartDatasets = [
+  //   { data: [48,40,40], 
+  //     label: 'Series A',
+  //     lineTension: 0.1,
+  //     pointRadius: 0,
+  //     fill: false
+  //   },
+  //   // { data: [28, 48, 40, 19, 86, 27, 90, 7], label: 'Series B' }
+  // ];
 
 
 
+  // onChartClick(event: MouseEvent): void { }
+
+  // onChartHover(event: MouseEvent): void { }
+
+  // addData(dataArr: Array<number>, label: string) {
+  //   this.barChartDatasets.forEach((dataset, index) => {
+  //     this.barChartDatasets[index] =
+  //       Object.assign({}, this.barChartDatasets[index], { data: [...this.barChartDatasets[index].data, dataArr[index]] });
+  //   });
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  public barChartOptions = {
-    scaleShowVerticalLines: false,
-    responsive: true, 
-    animation:{
-      duration: 250,
-      easing: 'linear'
-    },
-    scales: {
-      // xAxes: [{
-      //   type: 'time',
-      // }],
-      yAxes: [{
-        ticks: {
-          beginAtZero: true,
-          stepSize: 1
-        }
-      }]
-    }
-  };
-  public barChartLabels = ['first', 'second'];
-
-  public barChartType = 'line';
-  public barChartLegend = true;
-  public barChartDatasets = [
-    { data: [48,40,40], 
-      label: 'Series A',
-      lineTension: 0.1,
-      pointRadius: 0,
-      fill: false
-    },
-    // { data: [28, 48, 40, 19, 86, 27, 90, 7], label: 'Series B' }
-  ];
-
-
-
-  onChartClick(event: MouseEvent): void { }
-
-  onChartHover(event: MouseEvent): void { }
-
-  addData(dataArr: Array<number>, label: string) {
-    this.barChartDatasets.forEach((dataset, index) => {
-      this.barChartDatasets[index] =
-        Object.assign({}, this.barChartDatasets[index], { data: [...this.barChartDatasets[index].data, dataArr[index]] });
-    });
-
-
-    this.barChartLabels = [...this.barChartLabels, label];
-  };
+  //   this.barChartLabels = [...this.barChartLabels, label];
+  // };
 
   
 }
